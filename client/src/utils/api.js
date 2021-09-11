35b463150f8492c56358301773bd3c59b4a65f4c
arbhutale <tech1arb@gmail.com>
2021-09-11 16:45:13 +0530

    added card functionality

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import store from '../store';
 import { LOGOUT } from '../actions/types';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/json'
   }
